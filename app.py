daa9227c8e2dd0e25639130282c13fd607b07213
Remy93130 <remy93130@gmail.com>
2019-09-17 15:12:56 +0200

    Few change and testing the weebhook

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""Entry point from the application
+contains the different controller
+and return data needed according to
+the user request
+"""
+
 from flask import Flask, jsonify
 
 from services.data_service import DataService
@@ -7,6 +13,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """A beautiful hello world to test
+    the program because you can't bypass it"""
     return jsonify({"message": "Hello World!"})
 
 
--- services/data_service.py
+++ services/data_service.py
@@ -1,22 +1,44 @@
-"""Service who read csv file from the server
+""" Service who read csv file from the server
 the csv will be read with pandas and returns
 information needed by the user.
 """
 
 
 import pandas as pd
+import re
 
 
 class DataService:
-    """The main class for this service, used to manage the csv file and extract data.
+    """ The main class for this service, used to manage
+    the csv file and extract data.
     Give the csv path in parameter to build the class.
     """
     def __init__(self, data_file):
-        """
-        The class's constructor
-        :param data_file:
+        """ The class's constructor
+        :param data_file: The csv file
         """
         self.data_file = pd.read_csv(data_file)
+        self.__format_data()
 
-    def get_data_by_country(self, country_name):
+    def __format_data(self):
+        """ Format the data to obtain a readable
+        data frame with pandas and manipulate its easier
+        """
+        regex = r"\[.*\]"
+        self.data_file.describe(include="all")
+
+    def get_data_file(self):
+        """ Getter for the csv file
+        :return:
+        """
+        return self.data_file
+
+    def get_data_by_country(self, country_name, date=None):
+        """
+        :param country_name: The name of the country
+        :param date: The date from the data (default to None)
+        :return: dict
+        """
+        if country_name:
+            return self.data_file
         return None
