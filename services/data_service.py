05b97f15576b85635e457a26fe1b1c98ee40a5ca
Remy93130 <remy93130@gmail.com>
2019-10-01 17:35:37 +0200

    preparing data viz wisualisation with method to return dataframe and adding later information for geolocalisation

--- services/data_service.py
+++ services/data_service.py
@@ -21,6 +21,8 @@ class DataService:
 2011: (13000, 44), 2010: (12000, 42), 2009: (12000, 44), 2008: (11000, 40), \
 2007: (10000, 38), 2006: (11000, 42), 2005: (12000, 46), 2004: (12000, 51), \
 2003: (13000, 57), 2002: (12000, 56), 2001: (13000, 62), 2000: (14000, 67)}}
+    >>> service.get_data_by_country("Afghanistan", 3030)
+
     """
 
     def __init__(self, data_file):
@@ -58,13 +60,31 @@ class DataService:
             country_data = country_data[country_data.Year == date]
         if country_data.empty:
             return None
-        values = country_data.values.tolist()
         formatted_data = dict()
         formatted_data[country_name] = dict()
-        for value in values:
-            death_rate = (int(value[2]), int(value[3]))
-            formatted_data[country_name][value[1]] = death_rate
+        for value in country_data.values.tolist():
+            formatted_data[country_name][value[1]] = (int(value[2]), int(value[3]))
         return formatted_data
+
+    def get_data_for_visualisation(self, countries, dates=None):
+        """ Prepare the data for visualisation.
+        Return a pandas data frames filtered by the list of
+        countries and dates passed in arguments. If the
+        data frame is empty, return None
+        :param countries: The list of countries to select
+        :param dates: The list of dates to select
+        :return: Pandas data frame or None
+        >>> service = DataService('../static/raw_data.csv')
+        >>> service.get_data_for_visualisation(["Afghanistan"], [2010, 2011]).shape
+        (2, 4)
+        >>> service.get_data_for_visualisation(["Afghanistan"], [2010, 2011, 2012]).shape
+        (3, 4)
+        >>> service.get_data_for_visualisation(["Afghanistan"], [2040])
+        """
+        country_data = self.data_frame[self.data_frame.Country.isin(countries)]
+        if dates:
+            country_data = country_data[country_data.Year.isin(dates)]
+        return country_data if not country_data.empty else None
 
 
 if __name__ == '__main__':
