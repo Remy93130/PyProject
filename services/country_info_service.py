a0b58538c66a18f4428e74519853f552aeb2c5c9
Remy93130 <remy93130@gmail.com>
2019-10-01 23:06:28 +0200

    Finish method to complete the data frame + script for test data and country service

--- services/country_info_service.py
+++ services/country_info_service.py
@@ -2,10 +2,8 @@
 or a region with the restcountries api
 """
 
-
 import json
 import requests
-
 
 API_URL = "https://restcountries.eu/rest/v2/"
 
@@ -14,6 +12,7 @@ class CountryInfoService:
     """ Main class who contains methods to build
     requests for the API
     """
+
     def __init__(self):
         """ The class's constructor, use the
         global variable API_URL to get the entry point
@@ -50,4 +49,31 @@ class CountryInfoService:
         :return: dict
         """
         url = self.api_url + "region/" + region
-        return self.__do_request(url)
+        data = self.__do_request(url)
+        return data if 'status' in data and data['status'] == 404 else None
+
+    def complete_data_frame(self, data_frame):
+        """ Complete the data frame with information
+        who can be found with the countries api and
+        return a new data frame
+        :param data_frame: The original data frame
+        :return: The new data frame with more data
+        """
+        cached_data = dict()
+        rows_to_insert = {rows: [] for rows in ["Code", "Capital", "Population", "Size", "Gini"]}
+        for row in data_frame.iterrows():
+            frame = row[1]
+            if frame.Country not in cached_data:
+                cached_data[frame.Country] = self.get_information_by_name(frame.Country)
+            rows_to_insert["Code"].append(cached_data[frame.Country]["alpha3Code"])
+            rows_to_insert["Capital"].append(cached_data[frame.Country]["capital"])
+            rows_to_insert["Population"].append(cached_data[frame.Country]["population"])
+            rows_to_insert["Size"].append(cached_data[frame.Country]["area"])
+            rows_to_insert["Gini"].append(cached_data[frame.Country]["gini"])
+        for column, value in rows_to_insert.items():
+            data_frame.insert(len(data_frame.columns), column, value, True)
+        return data_frame
+
+
+if __name__ == '__main__':
+    pass
