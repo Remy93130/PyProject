25605b7c423794db335a198ed2f30e2ae168f6d2
Remy93130 <remy93130@gmail.com>
2019-10-20 16:23:51 +0200

    adding a route to create map and an another to access to them

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ the user request
 """
 
 import os
-from flask import Flask, jsonify
+import pathlib
+from flask import Flask, jsonify, send_from_directory
 from services.country_info_service import CountryInfoService
 from services.data_service import DataService
+from services.visualisation_service import VisualisationService
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
+
+CSV_PATH = "./static/raw_data.csv"
 
 
 @app.route('/')
@@ -46,9 +50,42 @@ def test_data():
     """ Function to display data in the csv file
     :return: html tab of the data
     """
-    service = DataService(os.getcwd() + "/static/raw_data.csv")
+    service = DataService(os.getcwd() + CSV_PATH)
     return service.get_data_frame().to_html()
 
 
+@app.route('/map/<int:date>')
+def test_world_map(date):
+    """ Function to create a map to the given argument
+    Check if the map is not already created else created it
+    and return the path to see it
+    :param date: The date to visualise data
+    :return: JSON data with the relative path
+    """
+    path = pathlib.Path('resources/map_{}.html'.format(date))
+    if not path.exists():
+        data_service = DataService(CSV_PATH)
+        country_service = CountryInfoService()
+        data_frame = data_service.get_data_for_visualisation(dates=[date])
+        data_frame = country_service.complete_data_frame(data_frame)
+        visualisation = VisualisationService(data_frame)
+        file = "./resources/map_{}.html".format(date)
+        visualisation.draw_world_map(
+            "number of deaths per 100,000 inhabitants in {}".format(date),
+            file
+        )
+    response = dict(path=str(path).replace("\\", "/"))
+    return jsonify(response)
+
+
+@app.route('/resource/<string:path>')
+def send_map_file(path):
+    """ Route to get maps
+    :param path: The html file
+    :return: The html file
+    """
+    return send_from_directory("resources", path)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
