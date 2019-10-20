87e9e56501ab6ccdbd5ecafeb5850eb900137677
Remy93130 <remy93130@gmail.com>
2019-10-20 15:12:35 +0200

    Creating viz service and testing with a file

--- services/visualisation_service.py
+++ services/visualisation_service.py
@@ -1,13 +1,46 @@
+""" Service to create different plot and save
+them in a folder given by the user
+"""
+
+
 import plotly.graph_objects as go
 import plotly.offline as ply
-import pandas as pd
 
 
 class VisualisationService:
-    """"""
+    """ Class to provide data visualisation.
+    Use the data frame given in constructor
+    to build different plot.
+    """
 
-    def __init__(self, data_file):
-        self.data = pd.read_csv(data_file)
+    def __init__(self, data):
+        self.data = data
 
-    def draw_world_map(self, title, date):
-        pass
+    def draw_world_map(self, title, path=""):
+        """ Use the data frame in the class to build an
+        interactive world map in javascript saved in a
+        html file. The file is saved in resources folder
+        and return the path to get it
+        :param path: The path to save the plot
+        :param title: The title to give to the plot
+        """
+        figure = go.Figure(data=go.Choropleth(
+            locations=self.data["Code"],
+            z=self.data["Deaths_per_100_000_population"],
+            text=self.data["Country"],
+            colorscale="hot",
+            autocolorscale=False,
+            reversescale=True,
+            marker_line_color="darkgray",
+            marker_line_width=.5,
+            colorbar_title="Deaths"
+        ))
+        figure.update_layout(
+            title_text=title,
+            geo=dict(
+                showframe=False,
+                showcoastlines=False,
+                projection_type="equirectangular"
+            )
+        )
+        ply.plot(figure, auto_open=False, filename=path)
--- viz.py
+++ viz.py
@@ -1,38 +1,11 @@
-import plotly.graph_objects as go
-import plotly.offline as ply
-import pandas as pd
-
 from services.data_service import DataService
 from services.country_info_service import CountryInfoService
-
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
+from services.visualisation_service import VisualisationService
 
 data = DataService("./static/raw_data.csv")
 info = CountryInfoService()
 df = data.get_data_for_visualisation(dates=[2016])
 df = info.complete_data_frame(df)
+viz = VisualisationService(df)
 
-
-fig = go.Figure(data=go.Choropleth(
-    locations=df['Code'],
-    z=df['Deaths_per_100_000_population'],
-    text=df['Country'],
-    colorscale='hot',
-    autocolorscale=False,
-    reversescale=True,
-    marker_line_color='darkgray',
-    marker_line_width=0.5,
-    colorbar_title='Deaths',
-))
-
-fig.update_layout(
-    title_text='Death by tuberculosis in 2017',
-    geo=dict(
-        showframe=False,
-        showcoastlines=False,
-        projection_type='equirectangular'
-    )
-)
-
-# fig.show()
-ply.plot(fig, filename="result_100.html")
+viz.draw_world_map("map_test", "./resources/map_test.html")
